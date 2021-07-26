b570721f464413376918111ce72056550bd5ae40
Nikos Timiopulos <nikos@timi.cz>
2021-07-26 10:44:46 +0200

    initial commit

---
+++ db/migrate/20210726081137_add_lives_to_animals.rb
@@ -0,0 +1,18 @@
+class AddLivesToAnimals < ActiveRecord::Migration[6.1]
+  def change
+    add_column :animals, :lives, :integer
+
+    reversible do |dir|
+      dir.up do
+        Animal.reset_column_information
+        Animal.all.each do |animal|
+          animal.lives = SecureRandom.random_number(10)
+        end
+      end
+
+      dir.down do
+        # nothing
+      end
+    end
+  end
+end
